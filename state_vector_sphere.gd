b1c798fe04ac26f19b72358fd07386ff653320a0
Baptiste Bayche <baptiste.bayche@etu.iut-tlse3.fr>
2023-09-30 23:12:41 +0200

    add floor in main

--- state_vector_sphere.gd
+++ state_vector_sphere.gd
@@ -4,6 +4,8 @@ var DIR = OS.get_executable_path().get_base_dir()
 var interpreter_path = DIR.plus_file("PythonFiles/venv/Scripts/activate")
 var script_path = DIR.plus_file("PythonFiles/bridging.py")
 
+var temperature = 0;
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	if !OS.has_feature("standalone"):
@@ -17,3 +19,9 @@ func compute_state_vector(a,b,c):
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+		
+func increase_temperature(degree):
+	temperature += degree
+	
+func decrease_temperature(degree):
+	temperature -= degree 
