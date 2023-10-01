6e1dafda2ea9a12c2ac15c751d021055d3e33745
Baptiste Bayche <baptiste.bayche@etu.iut-tlse3.fr>
2023-10-01 02:20:01 +0200

    add thermometer in main

--- thermometer.gd
+++ thermometer.gd
@@ -1,20 +1,31 @@
 extends Node2D
 
-
+var temperature
 
 
 # Fonction appelée au démarrage de la scène
 func _ready():
-	pass # Initialisez les nœuds du thermomètre
+	temperature = -269
 
 # Fonction pour augmenter la taille du rectangle vers le haut
-func increase_degree(degree : int):
+func increase_degree(degree : float):
 	# Augmentez la hauteur du rectangle
-	$degree.scale.y += degree
-	
-func decrease_degree(degree : int):
-	# Augmentez la hauteur du rectangle
-	$degree.scale.y -= degree
+	if temperature < -263 :
+		temperature += degree
+		$degree.scale.y += 0.07
+	else : 
+		get_tree().change_scene_to_file("res://menu.tscn")
+func decrease_degree(degree : float):	
+	if temperature > -269 :
+		$degree.scale.y -= 0.07
+		temperature -= degree
 
-func _on_button_pressed():
-	increase_degree(1)
+
+	
+func get_temperature():
+	return temperature
+
+
+func _on_timer_timeout():
+	increase_degree(0.1)
+	$Label.text = str("%.2f"%temperature) + "°C"
