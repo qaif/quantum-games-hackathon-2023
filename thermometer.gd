de4ef61277e7aaa07822f097a0cea35b2bb6edae
Baptiste Bayche <baptiste.bayche@etu.iut-tlse3.fr>
2023-10-01 05:28:53 +0200

    add mecanics shower

--- thermometer.gd
+++ thermometer.gd
@@ -1,6 +1,8 @@
 extends Node2D
 
 var temperature
+var in_shower = false
+
 
 # Fonction appelée au démarrage de la scène
 func _ready():
@@ -12,6 +14,7 @@ func increase_degree(degree : float):
 	if temperature < -263 :
 		temperature += degree
 		$degree.scale.y += 0.07
+		$Label.text = str("%.2f"%temperature) + "°C"
 	else : 
 		get_tree().change_scene_to_file("res://lose_menu.tscn")
 	
@@ -19,13 +22,18 @@ func decrease_degree(degree : float):
 	if temperature > -269 :
 		$degree.scale.y -= 0.07
 		temperature -= degree
+		$Label.text = str("%.2f"%temperature) + "°C"
 
-
-	
 func get_temperature():
 	return temperature
 
 
 func _on_timer_timeout():
-	increase_degree(0.1)
-	$Label.text = str("%.2f"%temperature) + "°C"
+	if !in_shower:
+		increase_degree(0.1)
+		$Label.text = str("%.2f"%temperature) + "°C"
+		
+
+	
+	
+
