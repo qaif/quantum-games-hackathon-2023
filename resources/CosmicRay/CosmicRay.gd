686878dfe7f1da8f916bde0aa3af729cd3fb918f
SimonR99 <simonroy99@hotmail.ca>
2023-10-02 10:51:47 -0400

    destroy cosmic ray out of viewport

--- resources/CosmicRay/CosmicRay.gd
+++ resources/CosmicRay/CosmicRay.gd
@@ -6,14 +6,18 @@ extends Node2D
 func _ready():
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	check_position()
 
 func _physics_process(delta):
 	position.y += speed*delta
 
+func check_position():
+	var viewport_size = get_viewport_rect().size
+	if position.y > viewport_size.y:
+		queue_free()  # This will delete the object from the scene
+
 func _on_rigid_body_2d_body_entered(body):
 	body.get_hit(1.0)
-	print("test")
+	print("Hit !")
