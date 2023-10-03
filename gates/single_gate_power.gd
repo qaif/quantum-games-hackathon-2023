7a0c98ce7a75021d3a8cc34d174f84e53c9f1f7a
Simon Roy <simonroy99@hotmail.ca>
2023-10-03 15:50:58 -0400

    Revert "Deploying to main from @ QuantumETS/EntangledExplorer@825c55beae114b2dd60d56761616fb8eeec7cf89 🚀"
    This reverts commit c56963be7a6188f3248df7482295839ae5e09210.

---
+++ gates/single_gate_power.gd
@@ -0,0 +1,33 @@
+@tool
+extends Node2D
+
+@export var letter = "H"
+@export var text_color : Color = Color(1, 1, 1, 1)
+@export var bg_color : Color = Color(0, 0, 0, 1)
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$Panel/Label.text = letter
+	$Panel/Label.add_theme_color_override("font_color", text_color)
+	$Panel.modulate = bg_color
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	$Panel/Label.text = letter
+	$Panel/Label.add_theme_color_override("font_color", text_color)
+	$Panel.modulate = bg_color
+
+	
+
+
+func _on_area_2d_body_entered(body):
+	var inventory = get_node("/root/inv")
+	if letter == "H":
+		inventory.hadamard += 1
+	if letter == "X":
+		inventory.paulix += 1
+	if letter == "Y":
+		inventory.pauliy += 1
+	if letter == "Z":
+		inventory.pauliz += 1
+	queue_free()
---
+++ state_vector_sphere.gd
@@ -0,0 +1,25 @@
+extends Node2D
+
+var DIR = OS.get_executable_path().get_base_dir()
+var interpreter_path = DIR.plus_file("PythonFiles/venv/Scripts/activate")
+var script_path = DIR.plus_file("PythonFiles/bridging.py")
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	if !OS.has_feature("standalone"):
+		interpreter_path = ProjectSettings.globalize_path("res://PythonFiles/venv/Scripts/activate")
+		script_path = ProjectSettings.globalize_path("PythonFiles/venv/Scripts/activate")
+	var gate_type = ["h","c","x","c","h"]
+	var gate_id = [0,0,0,1,1]
+	var circuit_size = 2
+	compute_state_vector(gate_type, gate_id, circuit_size)
+
+func compute_state_vector(gate_type,gate_id,size):
+	var output = []
+	OS.execute(interpreter_path, [script_path, gate_type, gate_id, size],output)
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+	
+
