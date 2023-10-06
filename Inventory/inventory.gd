3529901b661cab953c1e102c56bfdb98b6613465
SimonR99 <simonroy99@hotmail.ca>
2023-10-06 10:38:44 -0400

    Add inventory button

--- Inventory/inventory.gd
+++ Inventory/inventory.gd
@@ -5,18 +5,19 @@ extends Node2D
 func _ready():
 	pass # Replace with function body.
 
-var flipvar = 1
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	var inventory = get_node("/root/inv")
-	$H_label.text = str(inventory.hadamard) + " / 5"
-	$X_label.text = str(inventory.paulix) + " / 5"
-	$Y_label.text = str(inventory.pauliy) + " / 5"
-	$Z_label.text = str(inventory.pauliz) + " / 5"
+	$Inventory/H_label.text = str(inventory.hadamard) + " / 5"
+	$Inventory/X_label.text = str(inventory.paulix) + " / 5"
+	$Inventory/Y_label.text = str(inventory.pauliy) + " / 5"
+	$Inventory/Z_label.text = str(inventory.pauliz) + " / 5"
 	if Input.is_action_just_pressed("ui_filedialog_show_hidden"):
-		if (flipvar == 0):
-			show()
-			flipvar = 1
+		if (!$Inventory.visible):
+			$Inventory.show()
 		else:
-			flipvar = 0
-			hide()
+			$Inventory.hide()
+
+
+func _on_open_inventory_button_pressed():
+	$Inventory.show()
