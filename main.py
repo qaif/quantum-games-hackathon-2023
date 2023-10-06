da9e8925e81708463bc419a4f209446e6c649949
gczelusta <grzegorz.czelusta@doctoral.uj.edu.pl>
2023-10-06 21:10:32 +0200

    New gates and gates id

--- main.py
+++ main.py
@@ -60,10 +60,12 @@ class Bomb():
 
     def update_state(self, matrix):
         self.quantum_state = np.dot(matrix,self.quantum_state)
-        # self.quantum_state = [self.quantum_state[0] + coords[0], self.quantum_state[1] + coords[1]]
+        rho = np.angle(self.quantum_state[0])
+        if rho != 0:
+            self.quantum_state*=np.exp(-rho*1j)
+        self.quantum_state = np.real_if_close(self.quantum_state)
 
     def measurement(self):
-        # probability = np.power(np.sin(self.quantum_state[0]/2),2)
         probability = np.power(np.abs(self.quantum_state[0]),2)
         measurement_result = int(np.random.binomial(1, probability))
         return(measurement_result)
@@ -86,23 +88,27 @@ class Qubit(pygame.sprite.Sprite):
         self.surf = pygame.image.load(buffer).convert()
         self.rect = self.surf.get_rect(topright=(SCREEN_WIDTH,0))
 
+gates_ids = {'H':5, 'X':6, 'RY3':7, 'RY4':8, 'RZ2':9}
+gates_matrices = {
+    'H':1/np.sqrt(2)*np.array([[1,1],[1,-1]]),
+    'X':np.array([[0,1],[1,0]]),
+    'RY3':np.array([[np.cos(np.pi/6),-np.sin(np.pi/6)],[np.sin(np.pi/6),np.cos(np.pi/6)]]),
+    'RY4':np.array([[np.cos(np.pi/8),-np.sin(np.pi/8)],[np.sin(np.pi/8),np.cos(np.pi/8)]]),
+    'RZ2':np.array([[1,0],[0,np.exp(np.pi/2*1j)]])
+}
 
 class Gate(pygame.sprite.Sprite):
     def __init__(self, gate_type, center):
         super(Gate, self).__init__()
         self.gate_type = gate_type
-        self.matrix = None
-        if(gate_type == 'H'):
-            self.surf = pygame.image.load('imgs/gates/H.png')
-            self.matrix = 1/np.sqrt(2)*np.array([[1,1],[1,-1]])
-        elif(gate_type == 'X'):
-            self.surf = pygame.image.load('imgs/gates/X.png')
-            self.matrix = np.array([[0,1],[1,0]])
+        self.id = gates_ids[gate_type]
+        self.matrix = gates_matrices[gate_type]
+        self.surf = pygame.image.load('imgs/gates/'+gate_type+'.png')
         self.rect = self.surf.get_rect(center=center)
 
 def state_to_coords(state):
-    theta = 2*np.arccos(state[0])
-    phi = np.angle(state[1])
+    theta = np.real_if_close(2*np.arccos(state[0]))
+    phi = np.real_if_close(np.angle(state[1]))
     return [theta, phi]
 
 player = Player()
@@ -111,8 +117,9 @@ qubit = Qubit()
 grid = generate_grid(NUMBER_OF_TILES_X=NUMBER_OF_TILES_X, NUMBER_OF_TILES_Y=NUMBER_OF_TILES_Y)
 walls = grid_2_walls(grid, TILE_SIZE=TILE_SIZE)
 
-H = Gate('H', (200,200))
+H = Gate('H', (400,200))
 X = Gate('X',(350,200))
+RZ = Gate('RZ2',(700,200))
 
 H_boom = Gate('H', (500,200))
 X_boom = Gate('X',(650,200))
@@ -125,12 +132,14 @@ gates = pygame.sprite.Group()
 all_sprites.add(player)
 all_sprites.add(H)
 all_sprites.add(X)
+all_sprites.add(RZ)
 for w in walls:
     all_sprites.add(w)
 all_sprites.add(qubit)
 
 gates.add(H)
 gates.add(X)
+gates.add(RZ)
 
 
 clock = pygame.time.Clock()
