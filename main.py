802971fb3864ad1f1d3f2fe482fe984c27478669
Artur Miroszewski <a.miroszewski@gmail.com>
2023-10-06 15:50:23 +0200

    Add collisions

--- main.py
+++ main.py
@@ -11,8 +11,24 @@ pygame.init()
 SCREEN_WIDTH = 1920
 SCREEN_HEIGHT = 1080
 
+TILE_X_SIZE = 150
+TILE_Y_SIZE = 150
+NUMBER_OF_TILES_X = int(np.floor(SCREEN_WIDTH/TILE_X_SIZE))
+NUMBER_OF_TILES_Y = int(np.floor(SCREEN_WIDTH/TILE_Y_SIZE))
+
+grid = np.zeros((NUMBER_OF_TILES_Y,NUMBER_OF_TILES_X), dtype=int)
+
+
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))#, pygame.RESIZABLE)
 pygame.display.set_caption('Gra')
+
+def generate_grid():
+    grid = np.zeros((NUMBER_OF_TILES_Y,NUMBER_OF_TILES_X), dtype=int)
+    for i in range(grid.shape[0]):
+        for j in range(grid.shape[1]):
+            if((i==0 or j==0) or (i==(grid.shape[0]-1) or j==(grid.shape[1]-1))):
+                grid[i,j] = 1
+    return(grid)
 
 class Player(pygame.sprite.Sprite):
     def __init__(self):
@@ -23,25 +39,17 @@ class Player(pygame.sprite.Sprite):
         self.rect = self.surf.get_rect()
 
         
-    def update(self, pressed_keys):
+    def update_vertical(self, pressed_keys):
         if pressed_keys[K_UP]:
             self.rect.move_ip(0,-5)
         if pressed_keys[K_DOWN]:
             self.rect.move_ip(0,5)
+
+    def update_horizontal(self, pressed_keys):
         if pressed_keys[K_LEFT]:
             self.rect.move_ip(-5,0)
         if pressed_keys[K_RIGHT]:
             self.rect.move_ip(5,0)
-
-
-        if self.rect.left < 0:
-              self.rect.left = 0
-        if self.rect.right > SCREEN_WIDTH:
-              self.rect.right = SCREEN_WIDTH
-        if self.rect.top <= 0:
-              self.rect.top = 0
-        if self.rect.bottom >= SCREEN_HEIGHT:
-              self.rect.bottom = SCREEN_HEIGHT
 
 class Bomb():
     def __init__(self):
@@ -93,6 +101,13 @@ class Gate(pygame.sprite.Sprite):
             self.matrix = np.array([[0,1],[1,0]])
         self.rect = self.surf.get_rect(center=center)
 
+class Wall(pygame.sprite.Sprite):
+    def __init__(self):
+        super(Wall, self).__init__()
+        self.surf = pygame.Surface((75,75))
+        self.surf.fill((255,255,0))
+        self.rect = self.surf.get_rect(center=(400,400))
+
 def state_to_coords(state):
     theta = 2*np.arccos(state[0])
     phi = np.angle(state[1])
@@ -108,14 +123,19 @@ X_boom = Gate('X',(650,200))
 
 bomb = Bomb()
 
+wall = Wall()
+
 all_sprites = pygame.sprite.Group()
 gates = pygame.sprite.Group()
+walls = pygame.sprite.Group()
 all_sprites.add(player)
 all_sprites.add(qubit)
 all_sprites.add(H)
 all_sprites.add(X)
+all_sprites.add(wall)
 gates.add(H)
 gates.add(X)
+walls.add(wall)
 
 clock = pygame.time.Clock()
 
@@ -137,9 +157,20 @@ while running:
                 else:
                     all_sprites.add(X_boom)
 
+
     pressed_keys = pygame.key.get_pressed()
 
-    player.update(pressed_keys)
+    old_pos = player.rect.center
+    player.update_horizontal(pressed_keys)
+    wall_collision_horizontal = pygame.sprite.spritecollideany(player, walls)
+    if(wall_collision_horizontal):
+        player.rect.center = old_pos
+
+    old_pos = player.rect.center
+    player.update_vertical(pressed_keys)
+    wall_collision_vertical = pygame.sprite.spritecollideany(player, walls)
+    if(wall_collision_vertical):
+        player.rect.center = old_pos
 
     screen.fill((0,0,0))
     for entity in all_sprites:
@@ -152,6 +183,9 @@ while running:
         qubit.reLoadImage()
         gate_pass.kill()
 
+    #wall_collision = pygame.sprite.spritecollideany(player, walls)
+    #if(wall_collision):
+    #    player.rect.center = old_pos
 
     pygame.display.flip()
 
