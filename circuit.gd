1767e3fb90e71bf9d3f8c23f0baf9cd1360aa75a
Baptiste Bayche <baptiste.bayche@etu.iut-tlse3.fr>
2023-10-07 05:50:01 +0200

    complete circuit with gate

---
+++ circuit.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+var circuitOrder = ["H","CNOT","CNOT","H","Z","X"]
+var circuitIndex = 0;
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+	
+func nextFrame(gate):
+	if (gate == circuitOrder[circuitIndex]): 
+		circuitIndex += 1
+		$AnimatedSprite2D.frame = circuitIndex
+	else: 
+		get_tree().change_scene_to_file("res://lose_menu.tscn")
