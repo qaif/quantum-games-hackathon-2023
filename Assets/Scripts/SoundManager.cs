10b2911b5fa53c717ed69e68c4dfcd0b397083f9
Ihoold <ihoold@gmail.com>
2023-10-07 22:17:07 +0200

    Help window

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -7,15 +7,60 @@ public class SoundManager : MonoBehaviour
     static SoundManager instance_ = null;
     public SoundManager Instance => instance_ ?? (instance_ = FindObjectOfType<SoundManager>());
 
-    // Start is called before the first frame update
-    void Start()
-    {
+    public AudioSource audioSource;
 
+    [Header("Sound effects")]
+    public AudioClip bet;
+    public AudioClip call;
+    public AudioClip card;
+    public AudioClip click;
+    public AudioClip gate;
+    public AudioClip lose;
+    public AudioClip measure;
+    public AudioClip win;
+
+    [Header("Music")]
+    public AudioClip menu;
+    public AudioClip gameplay;
+    public AudioClip boss;
+
+    public void Bet()
+    {
+        audioSource.PlayOneShot(bet);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Call()
     {
+        audioSource.PlayOneShot(call);
+    }
 
+    public void Card()
+    {
+        audioSource.PlayOneShot(card);
+    }
+
+    public void Click()
+    {
+        audioSource.PlayOneShot(click);
+    }
+
+    public void Gate()
+    {
+        audioSource.PlayOneShot(gate);
+    }
+
+    public void Lose()
+    {
+        audioSource.PlayOneShot(lose);
+    }
+
+    public void Measure()
+    {
+        audioSource.PlayOneShot(measure);
+    }
+
+    public void Win()
+    {
+        audioSource.PlayOneShot(win);
     }
 }
